93d16d461d5f55d08a2986329b6cbe8c8ae0c670
Mujtaba <abbasimujtaba058@gmail.com>
2023-05-27 16:24:25 +0500

    Add PollDisplay Component commint3

--- src/PollDisplay.js
+++ src/PollDisplay.js
@@ -2,12 +2,10 @@ import React from "react";
 
 function PollDisplay(){
 
-    let sampleData = {
-
+    let sampleData = 
+        {
         "question": "What is your favorite programming language?",
-      
         "choices": [
-      
           { "id": 1, "label": "JavaScript", "votes": 0 },
       
           { "id": 2, "label": "Python", "votes": 0 },
@@ -18,11 +16,27 @@ function PollDisplay(){
         ]
     }
 
+    let arrayOFChoices = [...sampleData.choices]
+
+
+
 return (
     <>
     PollDisplay Component
     <br/>
-    {JSON.stringify(sampleData)}
+            <h3>
+                    {sampleData.question}
+            </h3>
+            <div>
+                {arrayOFChoices.map((item)=>{
+                    return <div>
+                            <li>{item.id}</li>
+                            <li>{item.label}</li>
+                            <button>{item.votes}</button>
+                    </div>
+                })}
+            </div>
+           
     </>
 )
 
