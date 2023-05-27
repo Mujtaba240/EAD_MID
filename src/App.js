881746e07eac843e89115787792bc0883213544d
Mujtaba <abbasimujtaba058@gmail.com>
2023-05-27 16:00:33 +0500

    Add PollDisplay Component commint2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import PollDisplay from "./PollDisplay"
 function App() {  
   return (
     <div className='App'>
     <h1>PollApp</h1>
+    <PollDisplay />
     
     </div>
   );
